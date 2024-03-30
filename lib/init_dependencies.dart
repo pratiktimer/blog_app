59e09f51a73b5a0399670085e2b4b6024eca2f84
praban <pbanawalkar2@gmail.com>
2024-03-30 21:35:30 +0530

    30 -03 -2024 completed

--- lib/init_dependencies.dart
+++ lib/init_dependencies.dart
@@ -7,7 +7,16 @@ import 'package:blog_app/features/auth/domain/usecases/current_user.dart';
 import 'package:blog_app/features/auth/domain/usecases/user_login.dart';
 import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:blog_app/features/blog/data/datasources/blog_local_data_source.dart';
+import 'package:blog_app/features/blog/data/datasources/blog_remote_data_source.dart';
+import 'package:blog_app/features/blog/data/repositories/blog_repository_impl.dart';
+import 'package:blog_app/features/blog/domain/repositories/blog_repository.dart';
+import 'package:blog_app/features/blog/domain/usecases/get_all_blogs.dart';
+import 'package:blog_app/features/blog/domain/usecases/upload_blog.dart';
+import 'package:blog_app/features/blog/presentation/bloc/bloc/blog_bloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 final serviceLocator = GetIt.instance;
@@ -19,7 +28,15 @@ Future<void> initDependencies() async {
   );
   _initAuth();
 
+  _initBlog();
+
+  Hive.defaultDirectory = (await getApplicationDocumentsDirectory()).path;
+
   serviceLocator.registerLazySingleton(() => Supabase.instance.client);
+
+  serviceLocator.registerLazySingleton(
+    () => Hive.box(name: 'blogs'),
+  );
 }
 
 void _initAuth() {
@@ -45,4 +62,44 @@ void _initAuth() {
       userLogin: serviceLocator(),
       currentUser: serviceLocator(),
       appUserCubit: serviceLocator()));
+}
+
+void _initBlog() {
+  // Datasource
+  serviceLocator
+    ..registerFactory<BlogRemoteDataSource>(
+      () => BlogRemoteDataSourceImpl(
+        serviceLocator(),
+      ),
+    )
+    ..registerFactory<BlogLocalDataSource>(
+      () => BlogLocalDataSourceImpl(
+        serviceLocator(),
+      ),
+    )
+    // Repository
+    ..registerFactory<BlogRepository>(
+      () => BlogRepositoryImpl(
+        serviceLocator(),
+        serviceLocator(),
+      ),
+    )
+    // Usecases
+    ..registerFactory(
+      () => UploadBlog(
+        serviceLocator(),
+      ),
+    )
+    ..registerFactory(
+      () => GetAllBlogs(
+        serviceLocator(),
+      ),
+    )
+    // Bloc
+    ..registerLazySingleton(
+      () => BlogBloc(
+        uploadBlog: serviceLocator(),
+        getAllBlogs: serviceLocator(),
+      ),
+    );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:blog_app/core/cubits/cubit/app_user_cubit.dart';
 import 'package:blog_app/core/theme/theme.dart';
 import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:blog_app/features/blog/presentation/bloc/bloc/blog_bloc.dart';
+import 'package:blog_app/features/blog/presentation/pages/bloc_page.dart';
 import 'package:blog_app/init_dependencies.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -11,7 +13,10 @@ Future<void> main() async {
   await initDependencies();
   runApp(MultiBlocProvider(providers: [
     BlocProvider(create: (_) => serviceLocator<AppUserCubit>()),
-    BlocProvider(create: (_) => serviceLocator<AuthBloc>())
+    BlocProvider(create: (_) => serviceLocator<AuthBloc>()),
+    BlocProvider(
+      create: (_) => serviceLocator<BlogBloc>(),
+    ),
   ], child: const MyApp()));
 }
 
@@ -41,11 +46,7 @@ class _MyAppState extends State<MyApp> {
         },
         builder: (context, isLoggedIn) {
           if (isLoggedIn) {
-            return const Scaffold(
-              body: Center(
-                child: Text('Logged in'),
-              ),
-            );
+            return const BlogPage();
           }
           return const SignInPage();
         },
